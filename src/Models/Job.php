3133453a1ec8af5647f1a8e92aa1a0794a4f4cda
Georgy <goshasafonov@yandex.ru>
2025-07-27 16:26:55 +0300

    fix

--- src/Models/Job.php
+++ src/Models/Job.php
@@ -4,12 +4,11 @@ namespace Yuges\Processable\Models;
 
 use Yuges\Package\Models\Model;
 use Yuges\Processable\Config\Config;
-use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Job extends Model
 {
-    use HasUuids, HasFactory;
+    use HasFactory;
 
     protected $table = 'jobs';
 
